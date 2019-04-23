a07b5740aa0e89447b050624af5ed66c309b23c9
roettger <3660889+snroettg@users.noreply.github.com>
2019-04-23 22:28:24 +0000

    mark install dirs as advanced

--- squish_export.h
+++ squish_export.h
@@ -1,4 +1,41 @@
-// empty export definitions to be overidden by cmake
-#ifndef SQUISH_EXPORT
-#   define SQUISH_EXPORT
+
+#ifndef SQUISH_EXPORT_H
+#define SQUISH_EXPORT_H
+
+#ifdef SQUISH_STATIC_DEFINE
+#  define SQUISH_EXPORT
+#  define SQUISH_NO_EXPORT
+#else
+#  ifndef SQUISH_EXPORT
+#    ifdef squish_EXPORTS
+        /* We are building this library */
+#      define SQUISH_EXPORT 
+#    else
+        /* We are using this library */
+#      define SQUISH_EXPORT 
+#    endif
+#  endif
+
+#  ifndef SQUISH_NO_EXPORT
+#    define SQUISH_NO_EXPORT 
+#  endif
+#endif
+
+#ifndef SQUISH_DEPRECATED
+#  define SQUISH_DEPRECATED __attribute__ ((__deprecated__))
+#endif
+
+#ifndef SQUISH_DEPRECATED_EXPORT
+#  define SQUISH_DEPRECATED_EXPORT SQUISH_EXPORT SQUISH_DEPRECATED
+#endif
+
+#ifndef SQUISH_DEPRECATED_NO_EXPORT
+#  define SQUISH_DEPRECATED_NO_EXPORT SQUISH_NO_EXPORT SQUISH_DEPRECATED
+#endif
+
+#define DEFINE_NO_DEPRECATED 0
+#if DEFINE_NO_DEPRECATED
+# define SQUISH_NO_DEPRECATED
+#endif
+
 #endif
